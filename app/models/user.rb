dfe7c6e0de66cc5b588d9daf03b94521fc13ca25
Eric Chiu <EricChiu920@gmail.com>
2019-07-30 11:27:22 -0400

    Fix no method for is_password? bug in find_by_credentials in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
 
   def self.find_by_credentials(username, password)
     @user = User.find_by(username: username)
-    return nil unless @user && self.is_password?(password)
+    return nil unless @user && @user.is_password?(password)
     @user
   end
 
