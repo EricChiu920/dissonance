10aa5e3908dd0a4c6c29c731d525e9c90c16f74d
Eric Chiu <EricChiu920@gmail.com>
2019-07-30 11:31:36 -0400

    Add session controller and view for login and signout

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,24 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login!(@user)
+      render :show
+    else
+      render json: ['Invalid username/password'], status: 404
+    end
+  end
+
+  def destroy
+    if current_user
+      logout!
+      render json: {}
+    else
+      render json: ['There was no logged in user'], status: 404
+    end
+  end
+end
