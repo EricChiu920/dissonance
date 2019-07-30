d4ccdf5aef20b443aab5ccc4e5381433465378a0
Eric Chiu <EricChiu920@gmail.com>
2019-07-30 15:17:43 -0400

    Start navbar and add login/logout links and buttons

---
+++ frontend/components/navbar/NavbarContainer.jsx
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { logout } from '../../actions/sessionActions';
+import Navbar from './Navbar';
+
+const mapStateToProps = (state) => {
+  const { session: { id }, entities: { users } } = state;
+  const currentUser = users[id];
+  return {
+    currentUser,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    logout: () => dispatch(logout()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
