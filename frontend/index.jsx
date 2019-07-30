e793f01d2618698aad08e6454eb1cc441932995b
Eric Chiu <EricChiu920@gmail.com>
2019-07-30 12:03:38 -0400

    Add ajax requests for login, logout, and signup

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+
+  /* TESTING */
+  window.login = login;
+  window.logout = logout;
+  window.signup = signup;
+  /* TESTING */
 
   ReactDOM.render(<h1>Welcome to dissonance</h1>, root);
 });
