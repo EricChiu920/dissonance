2afc0bc1823c9a8b67bd06571b40c1c4d2fbf59c
Eric Chiu <EricChiu920@gmail.com>
2019-07-31 13:54:46 -0400

    Add auth routes that redirect to root page if user is already logged in.

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
 import NavbarContainer from './navbar/NavbarContainer';
 import LoginFormContainer from './navbar/session/LoginFormContainer';
 import SignupFormContainer from './navbar/session/SignupFormContainer';
@@ -11,8 +12,8 @@ const App = () => (
         <Route exact path="/" component={NavbarContainer} />
       </nav>
     </header>
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <AuthRoute path="/login" component={LoginFormContainer} />
+    <AuthRoute path="/signup" component={SignupFormContainer} />
   </>
 );
 
