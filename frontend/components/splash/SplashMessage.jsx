bb33d82ca8a2d1c2c6712f0adf767a864de58274
Eric Chiu <EricChiu920@gmail.com>
2019-07-31 21:17:25 -0400

    Finish styling splash message and main images of splash page

--- frontend/components/splash/SplashMessage.jsx
+++ frontend/components/splash/SplashMessage.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SplashMessage = () => (
   <div className="splash-message">
-    <h3>It&apos;s time to ditch Skype and TeamSpeak</h3>
+    <h3>It&apos;s time to ditch Skype and TeamSpeak.</h3>
     <p>
       All-in-one voice and text chat for gamers that&apos;s free, secure, and
       works on both your desktop and phone.
+    </p>
+    <p>
       Stop paying for TeamSpeak servers and hassling with Skype.
       Simplify your life.
     </p>
     <span>
-      <button type="button">Login</button>
-      <button type="button">Login</button>
+      <Link to="/login">Login</Link>
+      <Link to="/login">Login</Link>
     </span>
   </div>
 );
