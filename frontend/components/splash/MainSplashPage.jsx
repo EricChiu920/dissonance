e6b95f9016fcdb32ae00bfbbced56485c81cccac
Eric Chiu <EricChiu920@gmail.com>
2019-07-31 21:28:10 -0400

    style mystery box

--- frontend/components/splash/MainSplashPage.jsx
+++ frontend/components/splash/MainSplashPage.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 const MainSplashPage = () => (
   <div className="splash-images-container">
+    {/* sub-background images */}
     <img src="https://discordapp.com/assets/a14c5b02487874dca7fae0481ef90dbb.svg" alt="cross" />
+
+    {/* sub images */}
+    <img className="bomb" src="https://discordapp.com/assets/eb301f28da3199edbd3ef19690d61674.svg" alt="bomb" />
+    <img src="https://discordapp.com/assets/9e05338bd66e0985fceb83317cb94b9c.svg" alt="coin" />
+    <img className="mystery-chest" src="https://discordapp.com/assets/81d74b2ebb053fbccee41865a47d48c3.svg" alt="mystery box" />
+
+    {/* main images */}
+    <img className="potion" src="https://discordapp.com/assets/0b5a0339571e72656eea93eb55d73eae.svg" alt="potion" />
     <img className="monitor" src="https://discordapp.com/assets/0d82411c439e3558f8b2f6fb12eccbc1.svg" alt="monitor with discord open" />
     <img className="disc" src="https://discordapp.com/assets/15149ecb9d5cd8faa24e1bbf45d70e5b.svg" alt="disc" />
     <img className="square-phone" src="https://discordapp.com/assets/5a31f41848bf3ba1817a092ac28c623d.svg" alt="phone with discord open" />
@@ -10,10 +19,6 @@ const MainSplashPage = () => (
     <img className="game-controller" src="https://discordapp.com/assets/c4bae281354a2b4e2db85415955e0994.svg" alt="game controller" />
     <img className="laptop" src="https://discordapp.com/assets/7edaed9d86e1b5dd9d4c98484372222b.svg" alt="loptop with discord open" />
     <img className="headphones" src="https://discordapp.com/assets/69db64955960eb333f5ff831cc1c0294.svg" alt="headphones that are hanging off the middle of the laptop" />
-
-    <img src="https://discordapp.com/assets/eb301f28da3199edbd3ef19690d61674.svg" alt="bomb" />
-    <img src="https://discordapp.com/assets/9e05338bd66e0985fceb83317cb94b9c.svg" alt="coin" />
-    <img src="https://discordapp.com/assets/81d74b2ebb053fbccee41865a47d48c3.svg" alt="mystery box" />
   </div>
 );
 
