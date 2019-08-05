e0a0e92581196e03c33bf45c8d7b9cf8c6593fbc
Eric Chiu <EricChiu920@gmail.com>
2019-08-04 20:08:23 -0400

    Add controller and views to join/leave a server

---
+++ app/controllers/api/user_servers_controller.rb
@@ -0,0 +1,26 @@
+class Api::UserServersController < ApplicationController
+  def create
+    @user_server = current_user.user_servers.new(user_server_params)
+
+    if user_server.save
+      render :show
+    else
+      render json: @user_server.errors.full_messages
+    end
+  end
+
+  def destroy
+    @user_server = current_user.user_servers.find_by(server_id: params[:id])
+
+    if @user_server
+      render :show
+    else
+      render json: ['You have not joined this server']
+    end
+  end
+
+  private
+  def user_server_params
+    params.require(:user_server).permit(:server_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :show]
     resources :servers, only: [:index, :show, :create, :update, :destroy]
+    resources :user_servers, only: [:create, :destroy]
 
     resource :session, only: [:create, :destroy]
   end
