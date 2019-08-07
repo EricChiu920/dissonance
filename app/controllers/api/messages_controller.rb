d02f847dee757d85bd715d4809b8ee2e04ef9fb7
Eric Chiu <EricChiu920@gmail.com>
2019-08-06 20:20:14 -0400

    Add controller actions for messages

---
+++ app/controllers/api/messages_controller.rb
@@ -0,0 +1,33 @@
+class Api::MessagesController < ApplicationController
+  def create
+    @message = current_user.messages.new(message_params)
+
+    if @message.save
+      render :show
+    else
+      render json: @message.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @message = current_user.messages.find(params[:id])
+
+    if @message.update(message_params)
+      render :show
+    else
+      render json: @message_params.error.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @message = current_user.messages.find(params[:id])
+
+    @message.destroy
+    render :show
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :channel_id, :edited)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :user_servers, only: [:create, :destroy]
     resources :servers, only: [:index, :show, :create, :update, :destroy]
     resources :channels, only: [:show, :create, :update, :destroy]
+    resources :messages, only: [:create, :update, :destroy]
 
     resource :session, only: [:create, :destroy]
   end
